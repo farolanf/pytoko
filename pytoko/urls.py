a0621bc21edf57edb58570532537bfa44eb85e57
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-04 11:29:37 +0800

    serve dev uploads

--- pytoko/urls.py
+++ pytoko/urls.py
@@ -29,7 +29,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    
+    from django.conf.urls.static import static
     import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+
+    urlpatterns = \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
