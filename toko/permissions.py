deb024513e309a598ce086885d264fecc32d613a
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-06 07:19:56 +0800

    refactor permission test

--- toko/permissions.py
+++ toko/permissions.py
@@ -5,6 +5,9 @@ class IsAdminOrSelf(permissions.BasePermission):
     Only admin or self allowed
     """
 
+    def has_permission(self, request, view):
+        return request.user.is_staff or request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or obj == request.user
 
@@ -12,6 +15,9 @@ class IsAdminOrOwner(permissions.BasePermission):
     """
     Only admin or owner allowed
     """
+
+    def has_permission(self, request, view):
+        return request.user.is_staff or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or obj.user == request.user
--- toko/tests/test_permission.py
+++ toko/tests/test_permission.py
@@ -4,33 +4,6 @@ from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_401_UN
 from .base import TestCase
 from .utils.user import User
 
-def can_action(method, detail, expected_status):
-    def test_method(self):
-        response = getattr(self.client, method)(self.detail_url if detail else self.base_url)
-        self.assertEqual(response.status_code, expected_status)
-    return test_method
-
-def cannot_action(method, detail):
-    def test_method(self):
-        expected_status = HTTP_403_FORBIDDEN if self.login else HTTP_401_UNAUTHORIZED
-        response = getattr(self.client, method)(self.detail_url if detail else self.base_url)
-        self.assertEqual(response.status_code, expected_status)
-    return test_method
-
-def inject_test_methods(cls, actions):
-    """
-    Create test methods on the derived class based on permission config.
-    
-    This is so test error reporting points to the derived class.
-    """
-    for action in cls.can:
-        kwargs = actions['can'][action]
-        setattr(cls, 'test_can_%s' % action, can_action(**kwargs))
-
-    for action in cls.cannot:
-        kwargs = actions['cannot'][action]
-        setattr(cls, 'test_cannot_%s' % action, cannot_action(**kwargs))
-
 actions = {
     'can': {
         'list': {
@@ -61,6 +34,7 @@ actions = {
         'my': {
             'method': 'get',
             'detail': False,
+            'as_param': True,
             'expected_status': HTTP_200_OK,
         },
     },
@@ -88,9 +62,47 @@ actions = {
         'my': {
             'method': 'get',
             'detail': False,
+            'as_param': True,
         },
     }
 }
+
+def can_action(action, method, detail, expected_status, as_param=False):
+
+    def test_method(self):
+        base_url = self.detail_url if detail else self.base_url
+        url = '%s%s/' % (base_url, action) if as_param else base_url
+        response = getattr(self.client, method)(url)
+        self.assertEqual(response.status_code, expected_status)
+
+    return test_method
+
+def cannot_action(action, method, detail, as_param=False):
+
+    def test_method(self):
+        base_url = self.detail_url if detail else self.base_url
+        url = '%s%s/' % (base_url, action) if as_param else base_url
+        expected_status = HTTP_403_FORBIDDEN if self.login else HTTP_401_UNAUTHORIZED
+        response = getattr(self.client, method)(url)
+        self.assertEqual(response.status_code, expected_status)
+
+    return test_method
+
+def inject_test_methods(cls):
+    """
+    Create test methods on the derived class based on permission config.
+    
+    This is so test error reporting points to the derived class.
+    """
+    for action in cls.can:
+        kwargs = actions['can'][action]
+        kwargs['action'] = action
+        setattr(cls, 'test_can_%s' % action, can_action(**kwargs))
+
+    for action in cls.cannot:
+        kwargs = actions['cannot'][action]
+        kwargs['action'] = action
+        setattr(cls, 'test_cannot_%s' % action, cannot_action(**kwargs))
 
 class PermissionMixin(object):
 
@@ -214,13 +226,13 @@ class AdOtherPermissionTest(AdsOtherPermissionTestBase):
     fixtures = ['users.json', 'taxonomy.json', 'regions.json', 'ads.json']
     role = Role.OTHER
 
-inject_test_methods(TaxonomyPublicPermissionTest, actions)
-inject_test_methods(TaxonomyAuthPermissionTest, actions)
+inject_test_methods(TaxonomyPublicPermissionTest)
+inject_test_methods(TaxonomyAuthPermissionTest)
 
-inject_test_methods(UserPublicPermissionTest, actions)
-inject_test_methods(UserSelfPermissionTest, actions)
-inject_test_methods(UserOtherPermissionTest, actions)
+inject_test_methods(UserPublicPermissionTest)
+inject_test_methods(UserSelfPermissionTest)
+inject_test_methods(UserOtherPermissionTest)
 
-inject_test_methods(AdPublicPermissionTest, actions)
-inject_test_methods(AdOwnerPermissionTest, actions)
-inject_test_methods(AdOtherPermissionTest, actions)
+inject_test_methods(AdPublicPermissionTest)
+inject_test_methods(AdOwnerPermissionTest)
+inject_test_methods(AdOtherPermissionTest)
