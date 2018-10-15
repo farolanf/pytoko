cee48819706788c37db9018a794e1e7b2fd1cd9d
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-15 21:03:49 +0800

    update forms

---
+++ toko/forms.py
@@ -0,0 +1,46 @@
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework import serializers
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+class FormView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    serializer_class = None
+    template = None
+    success_url = '/'
+
+    def get(self, request):
+        serializer = self.serializer_class()
+
+        return Response({
+            'serializer': serializer
+        }, template_name=self.template)
+
+    def post(self, request):
+        errors = None
+
+        self.serializer = self.serializer_class(data=request.data)
+                
+        if self.serializer.is_valid():
+            try:
+                self.form_valid(self.serializer.data)
+                return redirect(self.success_url)
+            except serializers.ValidationError as exc:
+                errors = exc.detail
+
+        return Response({
+            'errors': errors,
+            'serializer': self.serializer
+        }, template_name=self.template, status=HTTP_400_BAD_REQUEST)
+
+    def fail(self, msg, code=None):
+        raise serializers.ValidationError(msg, code)
+
+    def form_valid(self, data):
+        pass
+
+class AnonFormView(FormView):
+    authentication_classes = ()
+    permission_classes = ()
--- toko/views/user.py
+++ toko/views/user.py
@@ -4,56 +4,14 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.shortcuts import redirect
 from django.views.generic.edit import FormView
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import serializers
 
 from toko.serializers import LoginSerializer, RegisterSerializer
 from toko.utils.mail import send_mail
 from toko.utils.password import create_password_reset, do_password_reset
+from toko.forms import AnonFormView
 
 User = get_user_model()
-
-class FormView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    serializer_class = None
-    template = None
-    success_url = '/'
-
-    def get(self, request):
-        serializer = self.serializer_class()
-
-        return Response({
-            'serializer': serializer
-        }, template_name=self.template)
-
-    def post(self, request):
-        errors = None
-
-        self.serializer = self.serializer_class(data=request.data)
-                
-        if self.serializer.is_valid():
-            try:
-                self.form_valid(self.serializer.data)
-                return redirect(self.success_url)
-            except serializers.ValidationError as exc:
-                errors = exc.detail
-
-        return Response({
-            'errors': errors,
-            'serializer': self.serializer
-        }, template_name=self.template)
-
-    def fail(self, msg, code=None):
-        raise serializers.ValidationError(msg, code)
-
-    def form_valid(self, data):
-        pass
-
-class AnonFormView(FormView):
-    authentication_classes = ()
-    permission_classes = ()
 
 class LoginView(AnonFormView):
     serializer_class = LoginSerializer
