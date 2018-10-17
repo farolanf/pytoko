00ed0717e90c3900123b7c09e30adcb06008723e
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-17 13:16:07 +0800

    focus on element

--- static/toko/js/dom.js
+++ static/toko/js/dom.js
@@ -5,6 +5,7 @@ const utils = {
         const target = $('.field').toArray().find(el => $('.is-danger', el).length)
         if (!target) return
         this.scrollTo(target, -60)
+        $('input, textarea, select', target).focus()
     },
 
     scrollTo (target, offset = 0) {
