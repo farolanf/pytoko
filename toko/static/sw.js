1d4cdceb963d2d836e13e42ca82c16422b971a00
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-21 14:53:07 +0800

    cache only GET request

--- toko/static/sw.js
+++ toko/static/sw.js
@@ -2,7 +2,7 @@
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         fetch(e.request).then(function (response) {
-            if (e.request.url.startsWith('http')) {
+            if (e.request.method === 'GET' && e.request.url.startsWith('http')) {
                 const responseClone = response.clone()
                 caches.open('juwal').then(function (cache) {
                     cache.put(e.request, responseClone)
