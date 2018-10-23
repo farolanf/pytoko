9a5ab2c55bf4a5fb4041f286ed814fc1b5fda5cd
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-23 12:56:23 +0800

    bypass non get requests

--- toko/static/sw.js
+++ toko/static/sw.js
@@ -1,13 +1,14 @@
 
 self.addEventListener('fetch', function (e) {
+    if (! (e.request.method === 'GET' && e.request.url.startsWith('http'))) {
+        return
+    }
     e.respondWith(
         fetch(e.request).then(function (response) {
-            if (e.request.method === 'GET' && e.request.url.startsWith('http')) {
-                const responseClone = response.clone()
-                caches.open('juwal').then(function (cache) {
-                    cache.put(e.request, responseClone)
-                })
-            }
+            const responseClone = response.clone()
+            caches.open('get').then(function (cache) {
+                cache.put(e.request, responseClone)
+            })
             return response
         }).catch(function () {
             return caches.match(e.request)
