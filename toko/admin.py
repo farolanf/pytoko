fc5b9e797f8fafa48ddfadc5d94df188974fa83d
Farolan Faisal <farolanfaisal@gmail.com>
2018-10-29 21:58:04 +0800

    model: add product specs

--- toko/admin.py
+++ toko/admin.py
@@ -1,8 +1,38 @@
+from django import forms
 from django.contrib import admin
 from mptt.admin import DraggableMPTTAdmin
-from .models import User, Taxonomy
+from .models import User, Taxonomy, ProductType, Product, Value, Field, FieldValue, Ad
+
+class FieldForm(forms.ModelForm):
+
+    def __init__(self, *args, instance=None, **kwargs):
+        super().__init__(*args, instance=instance, **kwargs)
+        if instance:
+            self.fields['choices'].queryset = Value.objects.filter(group=instance.group)
+
+class FieldAdmin(admin.ModelAdmin):
+    form = FieldForm
+
+class FieldValueForm(forms.ModelForm):
+
+    def __init__(self, *args, instance=None, **kwargs):
+        super().__init__(*args, instance=instance, **kwargs)
+        if instance:
+            self.fields['value'].queryset = instance.field.choices.order_by('-value')
+
+class FieldValueAdmin(admin.ModelAdmin):
+    form = FieldValueForm
 
 admin.site.register(User)
 admin.site.register(Taxonomy, DraggableMPTTAdmin)
+
+admin.site.register(Value)
+admin.site.register(ProductType)
+admin.site.register(Product)
+admin.site.register(Ad)
+
+admin.site.register(Field, FieldAdmin)
+admin.site.register(FieldValue, FieldValueAdmin)
+
 
 # TODO: bulk add taxonomies from text, see laravel relevant code
--- toko/receivers.py
+++ toko/receivers.py
@@ -1,7 +1,18 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete
-from .models import File
+from django.db.models.signals import pre_delete, m2m_changed
+from .models import File, Product, Field
 
 @receiver(pre_delete, sender=File, dispatch_uid='delete_file')
 def delete_file(sender, instance, **kwargs):
     instance.file.delete()
+
+@receiver(m2m_changed, sender=Product.specs.through, dispatch_uid='unique_product_spec')
+def unique_product_spec(sender, instance, pk_set, action, reverse, model, **kwargs):
+    print(sender, action, reverse)
+    if action == 'pre_add':
+        if not reverse:
+            fields = Field.objects.filter(values__in=pk_set)
+            assert not sender.objects.filter(
+                product=instance, 
+                fieldvalue__field__in=fields
+            ).exists(), 'Duplicate specs'
