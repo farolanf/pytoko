e1a83984f9752deb03395339af889ffc78666aed
Farolan Faisal <farolanfaisal@gmail.com>
2018-11-01 16:53:54 +0800

    search: reinit sort-bar after pjax

--- toko/assets/components/vanillajs/sort-bar.js
+++ toko/assets/components/vanillajs/sort-bar.js
@@ -1,11 +1,19 @@
 once(function sortBar() {
     
-    $(".sort-bar").on("click", ".button", function(e) {
-        $(e.delegateTarget).toggleClass("is-active");
-    });
+    init()
 
-    $('.sort-bar .dropdown-menu .dropdown-item').on('click', function (e) {
-        const sortType = $(e.target).text()
-        utils.setUrl('/search/', { sort: sortType, page: 1 }, true)
-    })
+    $(document).on('pjax:end', init)
+
+    function init (e) {
+        const container = e && e.target || document
+
+        $(".sort-bar", container).on("click", ".button", function(e) {
+            $(e.delegateTarget).toggleClass("is-active");
+        });
+
+        $('.sort-bar .dropdown-menu .dropdown-item', container).on('click', function (e) {
+            const sortType = $(e.target).text()
+            utils.setUrl('/search/', { sort: sortType, page: 1 }, true)
+        })
+    }
 });
