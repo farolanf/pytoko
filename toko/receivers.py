771905ea8f8ba6f614a001653a8e8897a50372ee
Farolan Faisal <farolanfaisal@gmail.com>
2018-11-03 11:38:47 +0800

    add reset db

--- toko/receivers.py
+++ toko/receivers.py
@@ -1,17 +1,7 @@
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete, m2m_changed
-from .models import File, Product, Field
+from .models import File
 
 @receiver(pre_delete, sender=File, dispatch_uid='delete_file')
 def delete_file(sender, instance, **kwargs):
     instance.file.delete()
-
-@receiver(m2m_changed, sender=Product.specs.through, dispatch_uid='unique_product_spec')
-def unique_product_spec(sender, instance, pk_set, action, reverse, model, **kwargs):
-    if action == 'pre_add':
-        if not reverse:
-            fields = Field.objects.filter(values__in=pk_set)
-            assert not sender.objects.filter(
-                product=instance, 
-                fieldvalue__field__in=fields
-            ).exists(), 'Duplicate specs'
